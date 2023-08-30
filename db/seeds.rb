8f7a0865224274727e838835b3fb63621f33925c
ryusei1212 <black245k@gmail.com>
2023-08-30 08:41:10 +0000

    seedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,10 @@ User.create(
     name: 'テスト',
     email: 'test@example.com',
     password: 'password'
-    )
+)
+
+ToiletPlace.create(
+    user_id: User.first.id,
+    name: '綺麗なトイレ', 
+    address: '東京都大田区田園調布'
+)
