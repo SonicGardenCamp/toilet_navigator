af82b71bbbe453427344424de9a89035cb8a2de8
ryusei1212 <black245k@gmail.com>
2023-09-01 07:57:31 +0000

    トイレの登録フォームを修正

--- app/controllers/toilet_places_controller.rb
+++ app/controllers/toilet_places_controller.rb
@@ -14,7 +14,7 @@ class ToiletPlacesController < ApplicationController
     @toilet_place = ToiletPlace.new(toilet_place_params)
     @toilet_place.user_id = current_user.id
     
-    if @toilet_place.save!
+    if @toilet_place.save
       redirect_to root_path, notice: 'トイレの場所を登録しました'
     else
       render :new, status: :unprocessable_entity
