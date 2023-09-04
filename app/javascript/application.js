d4ac4d56ca348cb9d219b01708a1e70fe28131f2
Yasuo Masuyama <y.m19980526@gmail.com>
2023-09-04 16:07:17 +0900

    サイドバーとマップの表示改善

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function() {
+  var myTabs = new bootstrap.Tab(document.querySelector('#myTabs'))
+});
