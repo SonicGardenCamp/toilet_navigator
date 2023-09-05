8780f757cdf6add25244b4527553f16bd5f9ad33
Yasuo Masuyama <y.m19980526@gmail.com>
2023-09-05 11:05:27 +0900

    トイレの場所未登録時のエラー解消

--- app/models/toilet_place.rb
+++ app/models/toilet_place.rb
@@ -3,12 +3,19 @@ class ToiletPlace < ApplicationRecord
   has_many :reviews
   
   geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  before_validation :geocode, if: :address_changed?
   
   validates :name, presence: true
   validates :address, presence: true
+  validate :geolocation_present
   
   scope :default_order, -> { order(id: :asc) }
 
   private
+
+  def geolocation_present
+    if longitude.nil? || latitude.nil?
+      errors.add(:base, "正しい住所または名称を入力してください")
+    end
+  end
 end
