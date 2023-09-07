d9e4c0ff922903e390e9b800dc4e95e0cd3faa34
ryusei1212 <black245k@gmail.com>
2023-09-07 04:37:02 +0000

    音声で住所を検索できる機能を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./sppech"
 
 document.addEventListener("DOMContentLoaded", function() {
   var myTabs = new bootstrap.Tab(document.querySelector('#myTabs'))
---
+++ app/javascript/sppech.js
@@ -0,0 +1,14 @@
+const startBtn = document.querySelector('.js-startBtn');
+const search_form = document.forms.searchForm
+const addressInput = search_form.elements["q[address_cont]"];
+
+startBtn.addEventListener('click', () => {
+  SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+  const recognition = new SpeechRecognition();
+
+  recognition.start();
+
+  recognition.onresult = (event) => {
+    addressInput.value = event.results[0][0].transcript;
+  }
+})
